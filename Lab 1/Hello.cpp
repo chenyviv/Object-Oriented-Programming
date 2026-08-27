fcf6d1add0a680ea19ab7d5c7ebe4749da845682
Vivian Chen <vivianchen1@cpp.edu>
2026-08-26 22:39:42 -0700

    Add Hello.cpp to Lab 1

---
+++ Lab 1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+* {Coding Assignment 1 - Setting up IDE, Github, and Compile and Execute C++ Programs}
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    printf("Hello, World!\n");
+    return 0;
+}
